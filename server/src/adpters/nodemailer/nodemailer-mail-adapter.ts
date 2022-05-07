17a173ab36366111e025f7f2cf042c83564e750a
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2022-05-06 22:54:20 -0300

    Stage 3

---
+++ server/src/adpters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from '../mail-adapter';
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'df842a85a9451f',
+    pass: '0ce7517fa6925b',
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Matheus Ishiyama <matheus.ishiyama@outlook.com>',
+      subject,
+      html: body,
+    });
+  }
+}
